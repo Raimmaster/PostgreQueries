b6914c6e4c70e489317fa796171bcf86dc732775
Raim Interiano <rai.interiano@gmail.com>
2017-05-03 12:23:01 -0600

    Txt outputs of sql queries; sql file with sql for these queries;

--- postgre_main_selects.sql
+++ postgre_main_selects.sql
@@ -1,8 +1,18 @@
-select * from pg_tables;
+select * from pg_tables; --schemaname, tableowner
 select * from pg_index;
-select * from pg_indexes;
-select * from pg_proc;
-select * from pg_trigger;
+select * from pg_indexes;--''
+select * from pg_proc;--proname, proowner
+select * from pg_trigger;--tgname
 select * from pg_user;
+select * from pg_roles;--oid
+select * from pg_views;--'', viewowner
+select pg_get_functiondef(oid) from pg_proc; --full function def
+--INFORMATION_SCHEMA
 
-select * from pg_views;
+select * from information_schema.check_constraints; --constraint_schema
+select * from information_schema.referential_constraints; --FKs; constraint_schema
+select * from information_schema.schemata; --current db's accesable schemas
+select * from information_schema.tables; --table_catalog, table_schema
+select * from information_schema.triggers; --trigger_schema
+select * from information_schema.views; --table_catalog, table_schema
+
