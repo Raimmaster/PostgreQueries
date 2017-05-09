ac0342484f4587f1328c1f696975821a7480e57e
Raim <rai.interiano@gmail.com>
2017-05-09 02:00:11 -0600

    Specific queries to be used for DDLs generations; tables, columns, indexes, and checks

--- ProjectQueries.sql
+++ ProjectQueries.sql
@@ -1,5 +1,37 @@
 --Inciso 1 - Connections
-select * from pg_stat_activity;
+select pid from pg_stat_activity;
 select pg_terminate_backend( <procpid> );
 
 --Inciso 2 - DDL Tablas
+--TABLES
+select 
+	tab.schemaname, 
+	tab.tablename, 
+	tab.tablespace 
+from pg_tables tab;
+
+--COLUMNS
+select
+	cols.table_catalog, cols.table_schema, cols.table_name, cols.columns_name, cols.column_default, cols.is_nullable,
+	cols.data_type, cols.character_maximum_length, udt_name
+from information_schema.columns cols;
+
+--CHECKS
+select 
+	chks.constraint_catalog, chks.constraint_schema, chks.check_clause
+from information_schema.check_constraints chks;
+
+--PKs
+--missing column name
+select
+	refs_cons.constraint_catalog, refs_cons.constraint_schema, refs_cons.update_rule, refs_cons.delete_rule
+from information_schema.referential_constraints refs_cons;
+
+
+--FKs
+
+--REFERENCES constraint_catalog ()
+
+--Inciso 3 - Indexes
+--No PKs nor FKs
+select * from pg_indexes;
